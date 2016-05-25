2f1e2c0d528fa0a4e153804959a4408ffb658bda
Ahmed-Khalifa <ahmedkhalifa090@gmail.com>
2016-05-26 00:10:15 +0300

    fixed product name

--- cm.mk
+++ cm.mk
@@ -8,4 +8,4 @@ $(call inherit-product, device/samsung/a53g/full_a53g.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=a53gxx TARGET_DEVICE=a53g
 
-PRODUCT_NAME := cm_a53gxx
+PRODUCT_NAME := cm_a53g
